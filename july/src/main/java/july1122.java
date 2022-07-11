77d63b56efe792879a74338f0548388cf382c9dc
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-07-11 13:56:00 +0400

    Initial commit

---
+++ july/src/main/java/july1122.java
@@ -0,0 +1,259 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class july1122 {
+    public static void main(String[] args) {
+//        Person p1 = Person.createObject(33, "Ali");
+//        System.out.println(p1.toString());
+//        Person p2 = new Person(33, "ali");
+
+       /* Person p1 = new Person(4, "Ali");
+        System.out.println(p1 instanceof Children );*/
+        /*Children ch1 = new Children(1, "ali", "football");
+        Person ch1Person = (Person) ch1;
+        System.out.println(ch1.equals(ch1Person));*/
+
+        /*Children child1 = new Children(1, "Ali","volleyball");
+        Children child2 = new Children(1, "Ali","volleyball");
+        System.out.println("child1 hash code: "+child1.hashCode());
+        System.out.println("child2 hashcode: "+child2.hashCode());*/
+
+        /*Object child3 = new Children(1, "ALi", "basketball");
+        System.out.println(child3.getClass());*/
+
+       /* Children child1 = new Children(1, "Ali", new int[]{1, 2, 3}, "volleybol");
+        Children child2 = new Children(1, "Ali", new int[]{1, 2, 3}, "volleybol");
+        System.out.println("child1 hash code: "+child1.hashCode());
+        System.out.println("child2 hashcode: "+child2.hashCode());*/
+
+        /*A A1 = new A(1, 2);
+        A A2 = new A(1, 2);
+        B B1 = new B(A1, 2);
+        B B2 = new B(A2, 2);
+        System.out.println(B1.equals(B2));*/
+
+        A a1 = new B(1, 2);
+        a1.methodOverriden();
+    }
+}
+
+/*
+class Person {
+    *//*{
+        System.out.println("Non static block of parent class");
+    }*//*
+    public static int numberOfPeople;
+    public static String lastDatePersonAdded;
+    *//*static {
+//        Java classes aren't loaded into memory all at once, but when required by an application.
+        System.out.println("Static block of parent class");
+        numberOfPeople = 0;
+        lastDatePersonAdded = "Not added any person yet";
+    }*//*
+    private int age;
+    private String name;
+
+    private int[] arr;
+
+    Person(int age, String name, int[] arr) {
+        System.out.println("Person contructor");
+//        System.out.println(this.age);
+        this.age = age;
+        this.name = new String(name);
+        this.arr = arr;
+    }
+
+    Person() {
+        System.out.println("Parent class no arg constructor");
+    }
+
+    public static Person createObject(int age, String name, int[] arr) {
+        return new Person(age, name, arr);
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Age: "+this.age+"\nName: "+this.name+"\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return getAge() == person.getAge() && getName().equals(person.getName()) && Arrays.equals(arr, person.arr);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getAge(), getName());
+        result = 31 * result + Arrays.hashCode(arr);
+        return result;
+    }
+}
+
+class Children extends Person{
+    private String favoriteGame;
+    *//*{
+        System.out.println("non static block of child class");
+    }
+    static {
+        System.out.println("Static block of child class");
+    }*//*
+
+
+    Children(int age, String name, int[] arr, String game) {
+        super(age, name, arr);
+        favoriteGame = game;
+        System.out.println("Children Constructor.");
+    }
+
+    public String getFavoriteGame() {
+        return favoriteGame;
+    }
+
+    @Override
+    public int getAge() {
+        return super.getAge();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Children children = (Children) o;
+        return Objects.equals(getFavoriteGame(), children.getFavoriteGame());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getFavoriteGame());
+    }
+}*/
+
+/*
+class A {
+    int a;
+    int b;
+
+    A(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        A a1 = (A) o;
+        return getA() == a1.getA() && getB() == a1.getB();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getA(), getB());
+    }
+}
+
+class B implements Cloneable {
+    private A A1;
+    private int b;
+
+    public A getA1() {
+        return A1;
+    }
+
+    public void setA1(A a1) {
+        A1 = a1;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public B(A a1, int b) {
+        A1 = a1;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        B b1 = (B) o;
+        return getB() == b1.getB() && Objects.equals(getA1(), b1.getA1());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getA1(), getB());
+    }
+}*/
+
+class A {
+    private int a;
+
+    A(int a) {
+        this.a = a;
+    }
+
+    public void methodOverriden() {
+        System.out.println("Class A");
+    }
+}
+
+class B extends A {
+    private int b;
+
+    B(int a, int b) {
+        super(a);
+        this.b = b;
+    }
+
+    public void methodOverriden() {
+        System.out.println("Class B");
+    }
+}
---
+++ july/src/main/java/july722.java
@@ -0,0 +1,91 @@
+import java.security.Permission;
+import java.util.Arrays;
+import java.lang.Object;
+
+
+public class july722 {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+//        Object class methods extended
+        //.getClass()
+        Doggy dog1 = new Doggy("Alabash", 2, new int[]{1, 2, 3});
+//        System.out.println(dog1.getClass());
+
+
+        //hashCode()
+//        Doggy dog2 = dog1;
+//        System.out.println(dog1.equals(dog2));
+//        System.out.println(dog1.hashCode()+", "+dog2.hashCode());
+
+        //equals()
+//        dog1 = null;
+//        System.out.println(dog1.equals(dog1));
+//        Doggy dog2 = dog1;
+//        dog2.setAge(2);
+//        System.out.println(dog1.equals(dog2));
+
+//        String str1 = "Hello";
+//        String str2 = new String("Hello");
+//        System.out.println(str1.equals(str2));
+
+        // .clone()
+//        Doggy dog2 = dog1.doggyClone();
+//        System.out.println("Dog1: "+dog1.toString());
+//        System.out.println("Dog2: "+dog2.toString());
+//        dog1.changeArr(5);
+//        System.out.println(dog2.getDaysWorked()[0]);
+
+    }
+}
+
+class Doggy implements Cloneable {
+    private int[] daysWorked;
+    private String name;
+    private int age;
+
+    Doggy (String name, int age, int[] arr) {
+        this.name = name;
+        this.age = age;
+        this.daysWorked = arr;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int[] getDaysWorked() {
+        return daysWorked;
+    }
+
+    public void setDaysWorked(int[] daysWorked) {
+        this.daysWorked = daysWorked;
+    }
+
+    public void changeArr(int x) {
+        this.daysWorked[0] = x;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+       return "The dog's name is: "+this.getName()+"\nAge is "+this.getAge()+"\nDays worked: "+ Arrays.toString(this.daysWorked);
+    }
+
+}
