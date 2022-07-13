cf3bc832c168414259f10bb51aeee9f9955e1a6c
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-07-13 19:34:39 +0400

    started java basic - 6

---
+++ july/src/main/java/iyul1322.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+
+public class iyul1322 {
+    public static void main(String[] args) {
+        X x1 = new X();
+        System.out.println(x1.yOfX);
+        /*Point originOne = new Point(23, 94);
+        Rectangle rectOne = new Rectangle(originOne, 100, 200);
+        Rectangle rectTwo = new Rectangle(50, 100);
+
+
+        // display rectOne's width, height, and area
+        System.out.println("Width of rectOne: " + rectOne.width);
+        System.out.println("Height of rectOne: " + rectOne.height);
+        System.out.println("Area of rectOne: " + rectOne.getArea());
+
+        rectTwo.origin = originOne;
+
+        // display rectTwo's position
+        System.out.println("X Position of rectTwo: " + rectTwo.origin.x);
+        System.out.println("Y Position of rectTwo: " + rectTwo.origin.y);
+
+        rectTwo.move(40, 72);
+        System.out.println("X Position of rectTwo: " + rectTwo.origin.x);
+        System.out.println("Y Position of rectTwo: " + rectTwo.origin.y);
+        System.out.println(originOne.x);
+        //printing info about the objects*/
+    }
+}
+
+class X {
+    String name;
+    int x;
+    Y yOfX;
+
+    class Y {
+        String name;
+        int y;
+    }
+}
+
+
+ //codewars
+
+
+//object life cycle
+
+class Point {
+    public int x = 0;
+    public int y = 0;
+
+    Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
+
+class Rectangle {
+    public int width = 0;
+    public int height = 0;
+    public Point origin;
+
+    // four constructors
+    public Rectangle() {
+        origin = new Point(0, 0);
+    }
+    public Rectangle(Point p) {
+        origin = p;
+    }
+    public Rectangle(int w, int h) {
+        origin = new Point(0, 0);
+        width = w;
+        height = h;
+    }
+    public Rectangle(Point p, int w, int h) {
+        origin = p;
+        width = w;
+        height = h;
+    }
+
+    // a method for moving the rectangle
+    public void move(int x, int y) {
+        origin.x = x;
+        origin.y = y;
+    }
+
+    // a method for computing the area of the rectangle
+    public int getArea() {
+        return width * height;
+    }
+}
+
+
