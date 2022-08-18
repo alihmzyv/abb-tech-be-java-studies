4392cce53b559cf74874dcdc06de8130b3b10e85
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-08-18 15:52:36 +0400

    studied

--- july/src/main/java/StreamOrderingDemo/StreamsDemo.java
+++ july/src/main/java/StreamOrderingDemo/StreamsDemo.java
@@ -8,8 +8,14 @@ import java.util.stream.Collectors;
 
 public class StreamsDemo {
     public static void main(String[] args) {
-        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 15, 22, 11, 100);
-        int sum = integerList.stream().parallel().filter(i -> i > 10).collect(Collectors.summingInt(i -> i));
-        System.out.println(sum);
+//        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 15, 22, 11, 100);
+//        int sum = integerList.stream().parallel().filter(i -> i > 10).collect(Collectors.summingInt(i -> i));
+//        System.out.println(sum);
+//
+//        List<String> stringList = Arrays.asList("Dublicate", "Dozen", "delivery");
+//        //find the first word starting with d, case-insensitive
+//        String first = stringList.stream().filter(s -> s.charAt(0) == 'd' || s.charAt(0) == 'D').findFirst().orElse("None found");
+//        System.out.println(first);
+
     }
 }
