07f2ddeea7ea0b88a06a36124ac1e2106f62b433
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-08-18 20:10:36 +0400

    updated using stream

--- july/src/main/java/july2622/hackerrank/DesignerPDFViewer.java
+++ july/src/main/java/july2622/hackerrank/DesignerPDFViewer.java
@@ -5,19 +5,13 @@ import java.util.regex.Pattern;
 
 public class DesignerPDFViewer {
     public static int designerPdfViewer(List<Integer> h, String word) {
-        int[] letterHeights = h.stream().mapToInt(i->i).toArray(); //convert to array that is faster
-        word = word.replace(" ", ""); //remove spaces
-        int maxLetterHeight = letterHeights[word.charAt(0) - 97]; //assign to the height of first letter char initially
-        int alphabeticOrderOfLetter;
-
-        for (int i = 0; i < word.length(); i++) {
-            alphabeticOrderOfLetter = word.charAt(i) - 97; //starting from 0 for a
-
-            if (letterHeights[alphabeticOrderOfLetter] > maxLetterHeight) {
-                maxLetterHeight = letterHeights[alphabeticOrderOfLetter];
-            }
-        }
-
-        return maxLetterHeight * word.length();
+        int maxHeight = word.chars()
+                .unordered()
+                .parallel()
+                .map(i -> i - 97)
+                .map(h::get)
+                .max()
+                .getAsInt();
+        return maxHeight * word.length();
     }
 }
