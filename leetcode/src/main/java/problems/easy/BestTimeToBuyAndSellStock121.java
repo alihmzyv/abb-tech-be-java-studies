a300493ae01e5ac57e76ce9100f41a35833e74f6
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-08-20 12:14:58 +0400

    cant pass time limit test

--- leetcode/src/main/java/problems/easy/BestTimeToBuyAndSellStock121.java
+++ leetcode/src/main/java/problems/easy/BestTimeToBuyAndSellStock121.java
@@ -2,20 +2,16 @@ package problems.easy;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BestTimeToBuyAndSellStock121 {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-
-        for (int i = 0; i < prices.length - 1; i++) {
-            int[] buyablePrices = Arrays.copyOfRange(prices, i + 1, prices.length);
-            Arrays.sort(buyablePrices);
-            int possibleProfit = -prices[i] + buyablePrices[buyablePrices.length - 1];
-            if (possibleProfit > profit) {
-                profit = possibleProfit;
-            }
-        }
-
-        return profit;
+        List<Integer> pricesList = Arrays.stream(prices).boxed().collect(Collectors.toList());
+        return IntStream.range(0, prices.length - 1)
+                .map(i ->
+                        -pricesList.get(i) + pricesList.subList(i + 1, pricesList.size()).stream().max(Comparator.comparingInt(a -> a)).get())
+                .filter(profit -> profit > 0)
+                .max()
+                .orElse(0);
     }
 }
--- leetcode/src/main/java/problems/easy/Sqrt69.java
+++ leetcode/src/main/java/problems/easy/Sqrt69.java
@@ -1,12 +1,12 @@
 package problems.easy;
 
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
 public class Sqrt69 {
     public int mySqrt(int x) {
-        long result = 1;
-
-        for (; result * result <= x; result++) {
-        }
-
-        return (int) --result;
+        return (int) LongStream.rangeClosed(1, x)
+                .filter(i -> i * i <= x)
+                .max().getAsLong();
     }
 }
