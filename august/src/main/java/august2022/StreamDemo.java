3b194b98b2090d6f65421ee2e90c7ce7b3074bb5
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-08-20 12:15:35 +0400

    studied

---
+++ august/src/main/java/august2022/StreamDemo.java
@@ -0,0 +1,15 @@
+package august2022;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3, 5, 6));
+        System.out.println(integerList.stream()
+
+        );
+    }
+}
