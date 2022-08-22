51c1b8cafb9af7716c93e6de617568f9dde5e8c2
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-08-22 11:55:32 +0400

    better approach

--- leetcode/src/main/java/problems/easy/IsomorphicStrings205.java
+++ leetcode/src/main/java/problems/easy/IsomorphicStrings205.java
@@ -7,17 +7,17 @@ public class IsomorphicStrings205 {
     public boolean isIsomorphic(String s, String t) {
         Map<Character, Character> isoMap = new HashMap<>();
         return IntStream.range(0, s.length())
-                .noneMatch(i -> { //to short-circuit
+                .allMatch(i -> { //to short-circuit
                     Character found = isoMap.get(s.charAt(i));
                     if (found == null) { //no key matching
                         if (!isoMap.containsValue(t.charAt(i))) { //put and continue only if value not in map
                             isoMap.put(s.charAt(i), t.charAt(i));
-                            return false;
+                            return true;
                         }
-                        return true; //if there was key, value pair whose only pair is matching now
+                        return false; //if there was key, value pair whose only pair is matching now
                     }
                     else {
-                        return t.charAt(i) != found;
+                        return t.charAt(i) == found;
                     }
                 });
     }
